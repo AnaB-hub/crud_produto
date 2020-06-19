dfc2dd42472fd9770ecb8e1f8f225b215637c054
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-06-19 10:54:18 -0300

    Ajustes no Menu

--- crudProduto/src/app/app.module.ts
+++ crudProduto/src/app/app.module.ts
@@ -6,14 +6,17 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./app-routing.module";
 import { MaterialModule } from "./material/material.module";
 import { NavComponent } from "./components/nav/nav.component";
+import { ProdutoRoutingModule } from "./components/produto/produto-routing.module";
+import { CadastroProdutoComponent } from "./components/produto/cadastro-produto/cadastro-produto.component";
 
 @NgModule({
-  declarations: [AppComponent, NavComponent],
+  declarations: [AppComponent, NavComponent, CadastroProdutoComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     MaterialModule,
+    ProdutoRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
