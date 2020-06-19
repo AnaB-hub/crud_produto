ce06e26d61d1e57fd41f5cde75f95052aafe1864
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-06-19 20:58:11 -0300

    Adiciona listagem de produtos, juntamente com a alteração e exclusão

---
+++ crudProduto/src/app/components/produto/model/produto.ts
@@ -0,0 +1,9 @@
+export class Produto {
+  constructor(
+    public id?: number,
+    public nome?: string,
+    public qtde?: number,
+    public validade?: any,
+    public ativos?: boolean
+  ) {}
+}
---
+++ crudProduto/src/app/components/produto/produtos/produtos.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { ProdutoService } from "./../produto.service";
+import { MatSort, MatSnackBar, MatTableDataSource } from "@angular/material";
+import { Router } from "@angular/router";
+
+@Component({
+  selector: "app-produtos",
+  templateUrl: "./produtos.component.html",
+  styleUrls: ["./produtos.component.css"],
+})
+export class ProdutosComponent implements OnInit {
+  dados: any;
+  displayedColumns: string[] = ["nome", "qtde", "validade", "acoes"];
+
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+
+  constructor(
+    private produtoService: ProdutoService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
+
+  async ngOnInit() {
+    await this.carregarDados();
+    this.dados.sort = this.sort;
+  }
+
+  async carregarDados() {
+    let lista: any = await this.produtoService.consultar().toPromise();
+    this.dados = new MatTableDataSource(lista);
+  }
+
+  alterar(selecionado): void {
+    console.log(selecionado);
+    this.router.navigate([`/produto/edit/${selecionado}`]);
+  }
+
+  excluir(selecionado): void {
+    this.produtoService.excluir(selecionado).subscribe((_) => {
+      this.openCustomSnackBar("Produto excluído com sucesso!", "X");
+      this.carregarDados();
+    });
+  }
+
+  openCustomSnackBar(message: string, action) {
+    let snackBarRef = this.snackBar.open(message, action);
+  }
+}
