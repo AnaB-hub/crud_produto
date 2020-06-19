02f7e19cb7c7c2e78294587043eb16c3b78bd5d4
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-06-19 08:42:09 -0300

    Adiciona entidade de produto

---
+++ CRUD_produto/src/main/java/com/produto/entities/Produto.java
@@ -0,0 +1,63 @@
+package com.produto.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "produto")
+public class Produto implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+
+	private String nome;
+
+	private Integer qtde;
+
+	private Date validade;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getQtde() {
+		return qtde;
+	}
+
+	public void setQtde(Integer qtde) {
+		this.qtde = qtde;
+	}
+
+	public Date getValidade() {
+		return validade;
+	}
+
+	public void setValidade(Date validade) {
+		this.validade = validade;
+	}
+
+}
