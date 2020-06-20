6d4daa31b0aebcca3828fe7b64c2f1d7965c21b5
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-06-19 21:00:22 -0300

    Ajustes no menu

--- crudProduto/src/app/app.module.ts
+++ crudProduto/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { MAT_DATE_LOCALE } from "@angular/material";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./app-routing.module";
 import { MaterialModule } from "./material/material.module";
 import { NavComponent } from "./components/nav/nav.component";
 import { ProdutoRoutingModule } from "./components/produto/produto-routing.module";
 import { ProdutoModule } from "./components/produto/produto.module";
-import { MAT_DATE_LOCALE } from "@angular/material";
-import { ReactiveFormsModule, FormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
+import { PaginaInicialModule } from "./components/pagina-inicial/pagina-inicial.module";
+import { PaginaInicialRoutingModule } from "./components/pagina-inicial/pagina-inicial-routing.module";
 
 @NgModule({
   declarations: [AppComponent, NavComponent],
@@ -24,6 +26,8 @@ import { HttpClientModule } from "@angular/common/http";
     ProdutoRoutingModule,
     ProdutoModule,
     HttpClientModule,
+    PaginaInicialModule,
+    PaginaInicialRoutingModule,
   ],
   providers: [{ provide: MAT_DATE_LOCALE, useValue: "pt-BR" }],
   bootstrap: [AppComponent],
