6ae1f2205fa111b782f9b1cff0adc5f9fa4ef7aa
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-06-22 13:49:40 -0300

    Ajustes no cadastro e comenta código de exemplo usando map

--- CRUD_produto/src/main/java/com/produto/controller/ProdutoController.java
+++ CRUD_produto/src/main/java/com/produto/controller/ProdutoController.java
@@ -1,14 +1,12 @@
 package com.produto.controller;
 
-import java.util.HashMap;
-import java.util.Iterator;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
 import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -41,64 +39,7 @@ public class ProdutoController {
 	@GetMapping
 	@ApiOperation(value = "Lista de todos os produtos")
 	public List<Produto> produtos() {
-//		map();
 		return produtoRepository.findAll();
-	}
-
-	public void map() {
-		// Exemplo 1
-		Map<Integer, String> mapaNomes = new HashMap<Integer, String>();
-		mapaNomes.put(1, "João Delfino");
-		mapaNomes.put(2, "Maria do Carmo");
-		mapaNomes.put(3, "Claudinei Silva");
-		Set<Entry<Integer, String>> set = mapaNomes.entrySet();
-		Iterator<?> it = set.iterator();
-		while (it.hasNext()) {
-			Entry<Integer, String> entry = (Entry<Integer, String>) it.next();
-			System.out.println(entry.getKey() + "\t\t" + entry.getValue());
-		}
-		// Exemplo 2
-		Map<String, String> example = new HashMap<String, String>();
-		example.put("K1", new String("V1"));
-		example.put("K2", new String("V2"));
-		example.put("K3", new String("V3"));
-		example.put("K4", new String("V4"));
-		example.put("K5", new String("V5"));
-
-		String keyToSearch = "K1";
-		String valueToSearch = "V3";
-
-		if (example.containsKey(keyToSearch)) {
-			System.out.println("Valor da Chave " + keyToSearch + " = " + example.get(keyToSearch));
-		} else {
-			System.err.println("Chave não existe");
-		}
-		
-		if(example.containsValue(valueToSearch)) {
-			System.out.println("Chave: " + valueToSearch);
-		} else {
-			System.err.println("Chave não existe");
-		}
-		
-		//Exemplo 3
-		// Creating an empty HashMap 
-        HashMap<Integer, String> hash_map = new HashMap<Integer, String>(); 
-  
-        // Mapping string values to int keys 
-        hash_map.put(10, "Geeks"); 
-        hash_map.put(15, "4"); 
-        hash_map.put(20, "Geeks"); 
-        hash_map.put(25, "Welcomes"); 
-        hash_map.put(30, "You"); 
-  
-        // Displaying the HashMap 
-        System.out.println("Initial Mappings are: " + hash_map); 
-  
-        // Getting the value of 25 
-        System.out.println("The Value is: " + hash_map.get(25)); 
-  
-        // Getting the value of 10 
-        System.out.println("The Value is: " + hash_map.get(10)); 
 	}
 
 	@GetMapping("/ativos")
@@ -116,10 +57,11 @@ public class ProdutoController {
 
 	@PostMapping
 	@ApiOperation(value = "Salva produto")
-	public Produto saveProduto(@RequestBody ProdutoDTO dto) {
+	public ResponseEntity<Produto> saveProduto(@RequestBody ProdutoDTO dto) throws URISyntaxException {
 		Produto prod = mapper.setProdutoDTOTOProduto(dto);
 		prod.setAtivos(true);
-		return produtoRepository.save(prod);
+		produtoRepository.save(prod);
+		return ResponseEntity.created(new URI(("/produtos/salvar/" + prod.getNome()))).body(prod);
 	}
 
 	@PutMapping("/{id}")
@@ -136,5 +78,61 @@ public class ProdutoController {
 		produto.setAtivos(false);
 		produtoRepository.save(produto);
 	}
+	
+//	public void map() {
+		// Exemplo 1
+//		Map<Integer, String> mapaNomes = new HashMap<Integer, String>();
+//		mapaNomes.put(1, "João Delfino");
+//		mapaNomes.put(2, "Maria do Carmo");
+//		mapaNomes.put(3, "Claudinei Silva");
+//		Set<Entry<Integer, String>> set = mapaNomes.entrySet();
+//		Iterator<?> it = set.iterator();
+//		while (it.hasNext()) {
+//			Entry<Integer, String> entry = (Entry<Integer, String>) it.next();
+//			System.out.println(entry.getKey() + "\t\t" + entry.getValue());
+//		}
+		// Exemplo 2
+//		Map<String, String> example = new HashMap<String, String>();
+//		example.put("K1", new String("V1"));
+//		example.put("K2", new String("V2"));
+//		example.put("K3", new String("V3"));
+//		example.put("K4", new String("V4"));
+//		example.put("K5", new String("V5"));
+//
+//		String keyToSearch = "K1";
+//		String valueToSearch = "V3";
+//
+//		if (example.containsKey(keyToSearch)) {
+//			System.out.println("Valor da Chave " + keyToSearch + " = " + example.get(keyToSearch));
+//		} else {
+//			System.err.println("Chave não existe");
+//		}
+//		
+//		if(example.containsValue(valueToSearch)) {
+//			System.out.println("Chave: " + valueToSearch);
+//		} else {
+//			System.err.println("Chave não existe");
+//		}
+		
+		//Exemplo 3
+		// Creating an empty HashMap 
+//        HashMap<Integer, String> hash_map = new HashMap<Integer, String>(); 
+//  
+//        // Mapping string values to int keys 
+//        hash_map.put(10, "Geeks"); 
+//        hash_map.put(15, "4"); 
+//        hash_map.put(20, "Geeks"); 
+//        hash_map.put(25, "Welcomes"); 
+//        hash_map.put(30, "You"); 
+//  
+//        // Displaying the HashMap 
+//        System.out.println("Initial Mappings are: " + hash_map); 
+//  
+//        // Getting the value of 25 
+//        System.out.println("The Value is: " + hash_map.get(25)); 
+//  
+//        // Getting the value of 10 
+//        System.out.println("The Value is: " + hash_map.get(10)); 
+//	}
 
 }
